a84185a2b79d9f29de3ac45acf0529b8f90763a5
Jorge Pereyra <jorge.pereyra@DH.digitalhouse.com>
2024-08-08 21:09:51 -0300

    C11-Inicio Clinica Odontologica

---
+++ Clase 11- Inicio Clinica Odontologica/C11_ClinicaOdontologica/src/Dao/BD.java
@@ -0,0 +1,38 @@
+package Dao;
+
+import org.apache.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class BD {
+
+   private static final Logger logger= Logger.getLogger(BD.class);
+   private static final String SQL_DROP_CREATE_PACIENTES=" DROP TABLE IF EXISTS PACIENTES; " +
+           "CREATE TABLE PACIENTES(ID INT AUTO_INCREMENT PRIMARY KEY, NOMBRE VARCHAR(30) NOT NULL, APELLIDO VARCHAR(40) NOT NULL, " +
+           "CEDULA VARCHAR(30) NOT NULL, FECHA_INGRESO DATE NOT NULL, DOMICILIO_ID INT NOT NULL)";
+    private static final String SQL_DROP_CREATE_DOMICILIOS="DROP TABLE IF EXISTS DOMICILIOS; " +
+            "CREATE TABLE DOMICILIOS(ID INT AUTO_INCREMENT PRIMARY KEY, CALLE VARCHAR(100) NOT NULL, NUMERO INT NOT NULL, LOCALIDAD VARCHAR(100) NOT NULL, PROVINCIA VARCHAR(100) NOT NULL)";
+    private static final String SQL_DROP_PRUEBA="INSERT INTO DOMICILIOS (CALLE, NUMERO, LOCALIDAD, PROVINCIA) VALUES('Siempre Viva','748','Sprinfield','EUA'),('Calle Falsa','123','Sprinfield','EUA'); " +
+            "INSERT INTO PACIENTES (NOMBRE, APELLIDO, CEDULA, FECHA_INGRESO, DOMICILIO_ID) VALUES('Julian','Espinoza','234455667','2024-08-08',1),('Helen','Vasquez','3445666','2024-07-20',2)";
+    public static void crearTablas(){
+        Connection connection=null;
+        try{
+            connection= getConnection();
+            Statement statement= connection.createStatement();
+            statement.execute(SQL_DROP_CREATE_DOMICILIOS);
+            statement.execute(SQL_DROP_CREATE_PACIENTES);
+            statement.execute(SQL_DROP_PRUEBA);
+            logger.info("datos cargados con exito");
+        }catch (Exception e){
+            logger.error("error inesperado: "+e.getMessage());
+        }
+    }
+
+
+    public static Connection getConnection() throws Exception{
+        Class.forName("org.h2.Driver");
+        return DriverManager.getConnection("jdbc:h2:mem:~/ClinicaOdontologicaC2","sa","sa");
+    }
+}
---
+++ Clase 11- Inicio Clinica Odontologica/C11_ClinicaOdontologica/src/Dao/DomicilioDAOH2.java
@@ -0,0 +1,68 @@
+package Dao;
+
+import Model.Domicilio;
+import org.apache.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+
+public class DomicilioDAOH2 implements iDao<Domicilio>{
+    private static final Logger logger= Logger.getLogger(DomicilioDAOH2.class);
+    private static final String SQL_INSERT="INSERT INTO DOMICILIOS VALUES(?,?,?,?)";
+    private static final String SQL_SELECT_ONE="SELECT * FROM DOMICILIOS WHERE ID=?";
+
+
+
+    @Override
+    public Domicilio guardar(Domicilio domicilio) {
+        logger.info("iniciando las operaciones de : guardado de un domicilio: "+domicilio.getId());
+        Connection connection=null;
+        try{
+            connection=BD.getConnection();
+
+        }catch (Exception e){
+            logger.error("problemas con la BD"+e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public void actualizar(Domicilio domicilio) {
+
+    }
+
+    @Override
+    public void eliminar(Integer id) {
+
+    }
+
+    @Override
+    public List<Domicilio> listarTodos() {
+        return List.of();
+    }
+
+    @Override
+    public Domicilio buscarporId(Integer id) {
+        logger.info("iniciando las operaciones de : busqueda de un domicilio con ID: "+id);
+        Connection connection=null;
+        Domicilio domicilio=null;
+        try{
+            connection=BD.getConnection();
+            Statement statement= connection.createStatement();
+            PreparedStatement psSelectOne= connection.prepareStatement(SQL_SELECT_ONE);
+            psSelectOne.setInt(1,id);
+            psSelectOne.execute();
+            ResultSet rs= psSelectOne.executeQuery();
+            while(rs.next()){
+                domicilio= new Domicilio(rs.getInt(1),rs.getString(2),rs.getInt(3),rs.getString(4), rs.getString(5));
+            }
+
+        }catch (Exception e){
+            logger.error("problemas con la BD"+e.getMessage());
+        }
+        return domicilio;
+    }
+}
